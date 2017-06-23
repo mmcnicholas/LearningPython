f5e03304e732b551516417254d117fca602a1a6c
Unknown <mcnicholas1234@gmail.com>
2017-06-22 20:15:41 -0500

    Auto stash before merge of "master" and "origin/master"

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,1 +1,12 @@
-dictionary = {}
+from pprint import pprint
+
+dict1 = {}
+dict2 = {}
+
+dict1["Harry"] = "Potter"
+dict2["Hermione"] = "Granger"
+
+dict3 = dict1.copy()
+dict3.update(dict2)
+
+print(dict3)
